c5d4b8bd817de42984b73fc5357b19cb225775a9
Mike Minheere <99067548@mydavinci.nl>
2021-09-16 09:50:24 +0200

    kleine opdrachten

---
+++ tafelManieren/raketLancering.py
@@ -0,0 +1,4 @@
+import time
+for i in range(30,-1,-1):
+    print(i)
+    time.sleep(1)  
---
+++ tafelManieren/tijd.py
@@ -0,0 +1,9 @@
+import time
+
+for i in range(1,13,1):
+    print(str(i) + 'AM')
+    time.sleep(0.4)
+
+for i in range(1,13,1):
+    print(str(i) + 'PM')
+    time.sleep(0.4)
